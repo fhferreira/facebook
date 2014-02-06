c21c032a633230fb8c3da6a04c052f8254125706
gravitano <gue.warsono@gmail.com>
2014-02-06 16:03:14 +0700

    Update Namespace and First Commit

--- src/config/config.php
+++ src/config/config.php
@@ -1,15 +1,15 @@
 <?php 
 
 return array(
+	// AppId and app secret
 	'secret' => array(
-		//put your app id and secret
-								'appId'  => 'appId',
-						  	'secret' => 'secret'
-							),
+		'appId'  => 'appId',
+		'secret' => 'secret'
+	),
 	//Redirect after successfull login
-	'redirect' => 'YourSiteURL',
+	'redirect' 	=> 'http://localhost',
 	//When Someone Logout from your Site
-	'logout' => 'LogoutPageUrl',
+	'logout' 	=> 'http://localhost',
 	//you can add scope according to your requirement
-	'scope' => 'user_birthday,email,user_hometown,user_location,user_status,user_photos,user_likes,user_education_history'
-	);
+	'scope' 	=> 'user_birthday,email,user_hometown,user_location,user_status,user_photos,user_likes,user_education_history'
+);
